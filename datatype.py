a80bbda97eabbd2b7a8daf8ad31c957006ba16ab
herry2609 <ravalherry135@gmail.com>
2025-05-21 14:45:04 +0530

    completed all the datatypes.

--- datatype.py
+++ datatype.py
@@ -7,27 +7,32 @@
 a = 12
 print(a)  # the print() function is used to display output on the screen (console).
 print(type(a)) #type(obj) → returns the type of the object.
-#ANS :-  integer in terminal ( <class 'int'> )
+# output:- ( <class 'int'> )
+#ANS :-  integer 
 
 #(2) Float
 
 x = 3.14 #The value of pi.
 print(x)  # output :-  3.14
-print(type(x))   #ANS :-  float in terminal ( <class 'float'> )
+print(type(x))   # output:- ( <class 'float'> )
 
 #(3) Complex
 
 b = 3 + 2j # A complex number is a number that has two parts:(A real part) and (An imaginary part).
 print(b)   # output :- (3+2j)
 #here 3 is the real part, 2 is the imaginary part and j is the imaginary unit in Python (equivalent to i in mathematics).
-print(type(b))   #ANS :- complex in terminal ( <class 'complex'> )
+print(type(b))   # output:- ( <class 'complex'> )
+
+#----------------------------------------------------------------------
 
 ### 🔤 Text Type.
 
 # String
 y = "hello world" 'hello python'    # We use the (" ") double or (' ') single inverted comma.
 print(y) # output :- hello worldhello python
-print(type(y))   #ANS :-  string in terminal ( <class 'str'> )
+print(type(y))   # output:- ( <class 'str'> )
+
+#----------------------------------------------------------------------
 
 ### 📦 Sequence Types.
 
@@ -36,18 +41,74 @@ print(type(y))   #ANS :-  string in terminal ( <class 'str'> )
  
 c = [1,2,"hello",3.14]
 print(c)  # output :- [1,2,"hello",3.14]
-print(type(c))   #ANS :-  list in terminal ( <class 'list'> )
+print(type(c))   # output:- ( <class 'list'> )
 
 #(2) Tuple
 # denoted by ( '( )' ) square bracket.
 
 z = (10,3.14,"hello python!!")
 print(z) # output :- (10,3.14,"hello python!!")
-print(type(z))  #ANS :-  tuple in terminal ( <class 'tuple'> )
+print(type(z))  # output:- ( <class 'tuple'> )
 
 #(3) Range
 
 r = range(5)
 print(r)  # output :- (0,5)           
-print(type(r))  #ANS :-  range in terminal ( <class 'range'> )
+print(type(r))  # output:- ( <class 'range'> )
 
+#----------------------------------------------------------------------
+
+### 📘 Mapping Type.
+
+# Dictionary :- Collection of key-value pairs.
+#denoted by ( '{key:value}' ) curly bracket.
+
+dict1 = {"Name":"Herry","City":"Surat","Lang":"Gujarati"}
+print(dict1)    # output :- {"Name":"Herry","City":"Surat","Lang":"Gujarati"}
+print(type(dict1))  # output:- ( <class 'dict'> )
+
+#----------------------------------------------------------------------
+
+### 🔘 Set Types.
+
+#(1) Set 
+#denoted ( '{ }' )
+
+s1 = {1,3.14,"python","world"}
+print(s1) # Output :- {1,3.14,"python","world"}
+print(type(s1))     # output:- ( <class 'set'> )
+
+#(2) Frozenset
+
+# Creating a normal set
+s = {1, 2, 3}
+
+# Creating a frozenset from the set
+fs = frozenset(s)
+
+print(fs)           # Output: frozenset({1, 2, 3})
+print(type(fs))    # output:- ( <class 'frozenset'> ) 
+
+#----------------------------------------------------------------------
+
+### ❓ Boolean Type
+
+a = True
+b = False
+
+print(a)    # Output:- True
+print(b)    # Output:- False
+
+print(type(a))   # Output: (<class 'bool'>)
+print(type(b))   # Output: (<class 'bool'>)
+
+#----------------------------------------------------------------------
+
+### 🈳 None Type
+
+x = None
+
+print(x)             # Output: None
+print(type(x))       # Output: (<class 'NoneType'>)
+
+#----------------------------------------------------------------------
